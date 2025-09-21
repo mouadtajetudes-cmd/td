dc05e1bdbe5c8797a0027edc7fcd689fdd07c6a7
Mouad TAJ <mouadtaj.etudes@gmail.com>
2025-09-21 14:10:12 +0200

    Entités , Spécialite + Praticien

--- app/src/application_core/domain/entities/praticien/Praticien.php
+++ app/src/application_core/domain/entities/praticien/Praticien.php
@@ -2,9 +2,52 @@
 
 namespace toubilib\core\domain\entities\praticien;
 
+use Respect\Validation\Rules\Space;
 
 class Praticien
 {
- 
+private string $id; 
+private string $nom; 
+private string $prenom; 
+private string $ville; 
+private string $email; 
+private Specialite $specialite;
+
+public function  __construct(
+    string $id,
+    string $nom,
+    string $prenom, 
+    string $ville,
+    string $email,
+    Specialite $specialite
+    )
+{
+    $this->id=$id;
+    $this->nom=$nom;
+    $this->prenom=$prenom;
+    $this->ville=$ville;
+    $this->email=$email;
+    $this->specialite=$specialite;
+}
+
+    public function getId() : string{
+        return $this->id;
+    }
+    public function getNom() : string{
+        return $this->nom;
+    }
+    public function getPrenom() : string{
+        return $this->prenom;
+    }
+    public function getVille() : string{
+        return $this->ville;
+    }
+    public function getEmail() : string{
+        return $this->email;
+    }
+    public function getSpecialite() : Specialite{
+        return $this->specialite;
+    }
+
 
 }
--- app/src/application_core/domain/entities/praticien/Specialite.php
+++ app/src/application_core/domain/entities/praticien/Specialite.php
@@ -6,7 +6,25 @@ namespace toubilib\core\domain\entities\praticien;
 
 class Specialite
 {
+    private string $id;
+    private string $libelle;
+    private string $description;
 
- 
+    public function __construct(string $id, string $libelle, string $description)
+    {
+        $this->id=$id;
+        $this->libelle=$libelle;
+        $this->description=$description;
+    }
+
+    public function getId() : string{
+        return $this->id;
+    }
+    public function getLibelle() : string{
+        return $this->libelle;
+    }
+    public function getDescription() : string{
+        return $this->description;
+    }
 
 }
